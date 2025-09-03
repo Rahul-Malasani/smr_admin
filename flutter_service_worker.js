c627addc3b54661d252fa2be2cd2d0080015d5a5
Rahul-Malasani <rahulmalasani.47@gmail.com>
2025-09-04 02:45:43 +0530

    Fix: correct base href for GitHub Pages

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "b77eadb98119bdb6a045c329509c4994",
+const RESOURCES = {"flutter_bootstrap.js": "768c5107c0abb24e703a3389ba6a0bac",
 "version.json": "7372076184eaacac440d50a9743febc8",
 "index.html": "79e7118a6152bb9f2c6de18612499dd8",
 "/": "79e7118a6152bb9f2c6de18612499dd8",
